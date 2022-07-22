9571a704878c89fdbdab37f0644e543b9df022a5
Hubert <marshal604@gmail.com>
2022-07-22 10:21:22 +0800

    feat: init dom-based xss practice page

---
+++ nuxt/pages/dom-based-xss/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h1 class="text-4xl text-center">Dom-Based XSS</h1>
+    <div v-html="xssHref"></div>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      xssHref: '',
+    }
+  },
+  mounted() {
+    const { xssHref } = this.$route.query
+
+    this.xssHref = xssHref
+  },
+}
+</script>
